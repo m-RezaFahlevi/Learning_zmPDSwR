b9e85e643363f882789359c666f7507e63f9ba10
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-08-17 03:38:55 +0700

    Working with data from files

---
+++ Loading_data_into_R/rcodes.R
@@ -0,0 +1,35 @@
+#Initialize the data from the urls
+uciCar <- read.table('car.data.csv', sep = ',', header = TRUE, stringsAsFactors = TRUE)
+View(uciCar)
+#Examining our data
+class(uciCar)
+summary(uciCar)
+
+#Using R on less-structure data
+d <- read.table('https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data', sep = ' ', header = FALSE, stringsAsFactors = FALSE)
+View(d)
+colnames(d) <- c('Status.of.existing.checking.account',
+                'Duration.in.month', 'Credit.history', 'Purpose',
+                'Credit.amount', 'Savings account/bonds',
+                'Present.employment.since',
+                'Installment.rate.in.percentage.of.disposable.income',
+                'Personal.status.and.sex', 'Other.debtors/guarantors',
+                'Present.residence.since', 'Property', 'Age.in.years',
+                'Other.installment.plans', 'Housing',
+                'Number.of.existing.credits.at.this.bank', 'Job',
+                'Number.of.people.being.liable.to.provide.maintenance.for',
+                'Telephone', 'foreign.worker', 'Good.Loan')
+d$Good.Loan <- as.factor(ifelse(d$Good.Loan == 1, 'GoodLoan', 'BadLoan'))
+print(d[1:3,])
+source('scratch.r')
+dim(d)  #print the dimension of the table, row x columns
+class(d$Status.of.existing.checking.account)
+for (i in 1:dim(d)[2]) {
+    if (class(d[,i]) == "character") {
+        d[,i] <- as.factor(as.character(mapping[d[,i]]))
+    }
+}
+
+#Examining our new data
+table(d$Purpose, d$Good.Loan)
+summary(d)
